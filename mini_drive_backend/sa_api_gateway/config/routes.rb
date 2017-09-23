ce1f84af876b40ce9ec4f1e477dc423a85e61437
Sebastian Morales <sebastian_vem@hotmail.com>
2017-09-23 01:21:11 -0500

    update routes.rb

--- mini_drive_backend/sa_api_gateway/config/routes.rb
+++ mini_drive_backend/sa_api_gateway/config/routes.rb
@@ -1,19 +1,38 @@
 Rails.application.routes.draw do
  
+	
+	# Users Microservice
+	
 	get "user/validate"
 	post "user/createUser"
 	post "user/loginUser"
 	delete "user/logOut"
+	
+	# Upload Microservice
+
 	post "files/uploadFile"
 	post "files/uploadProof"
 	get "files/downloadFile"
 	get "files/listOfFiles"
 
 	# Categorize Microservice
+
 	get '/showCategories/file/:file_id', to:'categorize#showCategories'
 	delete '/categories/file/:file_id', to:'categorize#deleteAllCategories'
 	get '/showFiles/category/:category_name', to: 'categorize#getFilesWithCategory'
 	post '/addCategories/:file_id', to: 'categorize#addCategories'
 	post '/removeCategories/:file_id', to: 'categorize#removeCategories'
+ 
+	# Share Microservice
+
+	get "share/getShares"
+	post "share/postShares"
+	get "share/sharesById"
+	delete "share/delShare"
+
+	# Hash Microservice
+
+	get "hash/getHash"
+	post "hash/postHash"
 
 end
