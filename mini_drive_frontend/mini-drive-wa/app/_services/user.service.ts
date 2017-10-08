1f28302616f1cba61c95a85e74927ad9a1265058
Julián Jiménez <jcjimenezm@outsourcing.com.co>
2017-10-07 23:37:01 -0500

    [wa] Versionamiento aplicación web base para mini drive en angular

---
+++ mini_drive_frontend/mini-drive-wa/app/_services/user.service.ts
@@ -0,0 +1,56 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+import { User } from '../_models/index';
+import { UserRequest } from '../_models/index';
+
+@Injectable()
+export class UserService {
+    constructor(private http: Http) { }
+
+    getAll() {
+        return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
+    }
+
+    getById(id: number) {
+        return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    /*
+    create(user: User) {
+        return this.http.post('/api/users', user, this.jwt()).map((response: Response) => response.json());
+    }
+    */
+
+    //jucjimenezmo
+    create(user: UserRequest) {
+
+        const body = JSON.stringify(user);
+        console.log(body, 'user');
+
+        let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' });
+        headers.append('Access-Control-Allow-Origin', '*');
+        const options = new RequestOptions({ headers: headers });
+
+        return this.http.post('http://192.168.99.102:3000/users', body).map((response: Response) => response.json());
+    }
+
+    update(user: User) {
+        return this.http.put('/api/users/' + user.id, user, this.jwt()).map((response: Response) => response.json());
+    }
+
+    delete(id: number) {
+        return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // private helper methods
+
+    private jwt() {
+        // create authorization header with jwt token
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser && currentUser.token) {
+            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
+            return new RequestOptions({ headers: headers });
+        }
+    }
+}
