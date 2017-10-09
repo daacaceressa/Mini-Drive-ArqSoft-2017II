386240e22707bb9190d729dde726bc7f49804472
Daniel <daacaceressa@unal.edu.co>
2017-10-09 17:59:36 -0500

    Test Android Asynchronous Http Client library

--- FE/Mini-Drive/app/src/main/java/ii/a2017/arqsoft/minidrive/com/mini_drive/SignUpActivity.java
+++ FE/Mini-Drive/app/src/main/java/ii/a2017/arqsoft/minidrive/com/mini_drive/SignUpActivity.java
@@ -1,5 +1,6 @@
 package ii.a2017.arqsoft.minidrive.com.mini_drive;
 
+import com.loopj.android.http.*;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -30,7 +41,52 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         switch(v.getId()) {
             case R.id.signUpButton:
+                /*try {
+                    URL url = new URL( getString( R.string.API_URL ) + "/users" );
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestMethod( "POST" );
+                    urlConnection.setRequestProperty("email", email.toString());
+                    urlConnection.setRequestProperty("password", email.toString());
+                    urlConnection.setRequestProperty("password_confirmation", passwordConfirmation.toString());
+                    urlConnection.connect();
+                    Toast.makeText( getApplicationContext(), urlConnection.getResponseCode()+"", Toast.LENGTH_SHORT).show();
+                    if( urlConnection.getResponseCode() == 201 ){
 
+                    }
+                    else {
+
+                    }
+                    urlConnection.disconnect();
+                } catch (Exception e) {
+                    Toast.makeText( getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }*/
+                AsyncHttpClient client = new AsyncHttpClient();
+                client.get(getString( R.string.API_URL ), new AsyncHttpResponseHandler() {
+
+                    @Override
+                    public void onStart() {
+                        //Toast.makeText( getApplicationContext(), "Hola", Toast.LENGTH_SHORT).show();
+                        // called before request is started
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                        Toast.makeText( getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
+                        // called when response HTTP status is "200 OK"
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                        Toast.makeText( getApplicationContext(), "La puta que me pario", Toast.LENGTH_SHORT).show();
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    }
+
+                    @Override
+                    public void onRetry(int retryNo) {
+                        // called when request is retried
+                    }
+                });
                 break;
 
         }
