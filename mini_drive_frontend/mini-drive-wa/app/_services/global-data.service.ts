0cfe9a049800c1cea9dcd91fb46f96aff8ab9a20
jucjimenezmo <jucjimenezmo@unal.edu.co>
2017-11-13 23:06:37 -0500

    ip_base to load balancer

--- mini_drive_frontend/mini-drive-wa/app/_services/global-data.service.ts
+++ mini_drive_frontend/mini-drive-wa/app/_services/global-data.service.ts
@@ -4,6 +4,11 @@ import { Injectable } from '@angular/core';
 export class GlobalDataService {
 
     //this parameter is used in all services, preview.component.ts and upload.component.ts files
-    base_ip: string = 'http://35.188.6.128:4000';
+
+    //api-gateway google platform
+    // base_ip: string = 'http://35.188.6.128:4000';
+
+    //balanceador de carga
+    base_ip: string = 'http://35.202.138.154:4000';
 
 }
